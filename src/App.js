93652571ce30cb8290252eeff2447d1e67ef83d5
Alexandr <awpogodin@mail.ru>
2020-01-15 13:22:09 +0400

    Add localStorage for storing todos

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Header from "./components/Header.component";
 import TodoList from "./components/TodoList.component";
 import AddTodo from "./components/AddTodo.component";
 
+const STORAGE_TODOS = 'todos';
+
 class App extends React.Component {
     constructor(props) {
         super(props);
@@ -11,17 +13,18 @@ class App extends React.Component {
         this.state = {
             todos: [],
             loading: true
-        }
+        };
+
+        this.localTodos = [];
     };
 
     componentDidMount() {
+        this.localTodos = JSON.parse(localStorage.getItem(STORAGE_TODOS)) || [];
         setTimeout(() => {
             this.setState(() => {
                 return {
                     todos: [
-                        {id: 1, title: 'Купить хлеб', completed: false},
-                        {id: 2, title: 'Купить молоко', completed: false},
-                        {id: 3, title: 'Купить сок', completed: false},
+                        ...this.localTodos
                     ],
                     loading: false
                 }
@@ -29,38 +32,51 @@ class App extends React.Component {
         }, 2000);
     }
 
+    updateLocalStorage = () => {
+        console.log(this.localTodos);
+        localStorage.setItem(STORAGE_TODOS, JSON.stringify(this.localTodos));
+    };
+
     onToggle = (id) => {
+        const toggleTodos = (todo) => {
+            if (todo.id === id) {
+                return {...todo, completed: !todo.completed}
+            }
+            return todo;
+        };
+        this.localTodos = this.localTodos.map(todo => toggleTodos(todo));
         this.setState(prevState => {
             return {
-                todos: prevState.todos.map(todo => {
-                    if (todo.id === id) {
-                        return {...todo, completed: !todo.completed}
-                    }
-                    return todo;
-                })
+                todos: prevState.todos.map(todo => toggleTodos(todo))
             }
-        })
+        });
+        this.updateLocalStorage();
     };
 
     onAdd = (title) => {
         if (title.trim()) {
+            const todo = {id: Date.now(), title, completed: false};
+            this.localTodos.push(todo);
             this.setState(prevState => {
                 return {
                     todos: [
                         ...prevState.todos,
-                        {id: Date.now(), title, completed: false}
+                        todo
                     ]
                 }
-            })
+            });
+            this.updateLocalStorage();
         }
     };
 
     onDelete = id => {
+        this.localTodos = this.localTodos.filter(todo => todo.id !== id);
         this.setState(prevState => {
             return {
                 todos: prevState.todos.filter(todo => todo.id !== id)
             }
-        })
+        });
+        this.updateLocalStorage();
     };
 
     render() {
